ef7998616a376d966fe819ff706801e50e070439
zzq <zhuzhengqing321@qq.com>
2019-10-10 20:39:17 +1100

    Secure Pipe complete.

---
+++ src/security/SecurePipe.java
@@ -0,0 +1,93 @@
+package security;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class SecurePipe {
+    /*
+     * encryption
+     */
+    public static String AESEncode(String encodeRules, String content) {
+        try {
+            KeyGenerator keygen = KeyGenerator.getInstance("AES");
+            keygen.init(128, new SecureRandom(encodeRules.getBytes()));
+            SecretKey original_key = keygen.generateKey();
+            byte[] raw = original_key.getEncoded();
+            SecretKey key = new SecretKeySpec(raw, "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] byte_encode = content.getBytes("utf-8");
+            byte[] byte_AES = cipher.doFinal(byte_encode);
+            String AES_encode = new String(
+                    new BASE64Encoder().encode(byte_AES));
+            return AES_encode;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        // if something wrong return null
+        return null;
+    }
+
+    /*
+     * decryption
+     */
+    public static String AESDncode(String encodeRules, String content) {
+        try {
+            KeyGenerator keygen = KeyGenerator.getInstance("AES");
+            keygen.init(128, new SecureRandom(encodeRules.getBytes()));
+            SecretKey original_key = keygen.generateKey();
+            byte[] raw = original_key.getEncoded();
+            SecretKey key = new SecretKeySpec(raw, "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] byte_content = new BASE64Decoder().decodeBuffer(content);
+
+            /*
+             * decrypt
+             */
+            byte[] byte_decode = cipher.doFinal(byte_content);
+            String AES_decode = new String(byte_decode, "utf-8");
+            return AES_decode;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+
+        // if something wrong return null
+        return null;
+    }
+}
